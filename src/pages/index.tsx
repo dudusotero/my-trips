f229d3bb46b728f3a970cf0b58c86b968237ffe9
Eduardo Sotero <eduardoliberatosotero@gmail.com>
2021-03-04 00:54:11 -0300

    First commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Home</h1>
+    </>
+  )
+}
+
+export default Home
